602ce96d7e0919bbb33bc7fd07760de57683034d
mauro Leos <mauroleos94@gmail.com>
2020-04-08 23:59:29 -0500

    Added scoreboard

--- scripts.js
+++ scripts.js
@@ -3,36 +3,34 @@ let userChoice
 var results = finalResults()
 const displayResults = document.getElementById("results");
 const computerAnswer = document.getElementById("computer-choice");
-const randomNumber = Math.floor(Math.random() * 4);
 const userAnswer = document.getElementById("user-choice");
 const possibleChoices = document.querySelectorAll(".choices");
 const restartBtn = document.getElementById("restart-button");
-
-// Restart Game
-restartBtn.addEventListener('click', () => {
-    document.location.reload();
-});
+let userScore = 0;
+let computerScore = 0;
 
 // Get userChoice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id
     randomComputerChoice();
     finalResults();
+    getScore();
     computerAnswer.innerHTML = computerChoice
     userAnswer.innerHTML = userChoice
     displayResults.innerHTML = results
-    // setTimeout (restartGame, 3000);
 }))
 
 //Get a random computerChoice number
 let randomComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 4);
     if (randomNumber === 1) {
-        return computerChoice = 'rock'
+        computerChoice = 'rock'
     } else if (randomNumber === 2) {
-        return computerChoice = 'paper'
-    } else if (randomNumber === 3) {
-        return computerChoice = 'scissors'
+        computerChoice = 'paper'
+    } else {
+        computerChoice = 'scissors'
     }
+    return computerChoice;
 }
 
 // Get results
@@ -52,4 +50,26 @@ function finalResults() {
     } else if (computerChoice === 'scissors' && userChoice === 'paper') {
         return results = 'You lost'
     }
+}
+
+//Get Scoreboard
+let getScore = () => {
+    if (computerChoice == userChoice) {
+        return results = "It's a tie!"
+    } else if (computerChoice === 'rock' && userChoice === 'paper') {
+        userScore++, computerScore + 0;
+    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
+        userScore + 0, computerScore++;
+    } else if (computerChoice === 'paper' && userChoice === 'rock') {
+        userScore + 0, computerScore++;
+    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
+        userScore++, computerScore = 0;
+    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
+        userScore++, computerScore = 0;
+    } else if (computerChoice === 'scissors' && userChoice === 'paper') {
+        userScore + 0, computerScore++;
+    }
+
+    document.getElementById("computer-score").innerHTML = "Computer Score: " + computerScore;
+    document.getElementById("user-score").innerHTML = "Your Score: " + userScore;
 }
