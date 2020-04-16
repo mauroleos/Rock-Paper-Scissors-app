3c9726469d9f7039337abd64069d91eff658fd11
mauro Leos <mauroleos94@gmail.com>
2020-04-15 22:18:46 -0500

    Cleaned up code

--- scripts.js
+++ scripts.js
@@ -1,27 +1,22 @@
-const possibleChoices = document.querySelectorAll(".choices");
-const restartBtn = document.getElementById("restart-button");
 let userScore = 0;
 let computerScore = 0;
 
 // Get userChoice
+const possibleChoices = document.querySelectorAll(".choices");
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id;
+    const userChoice = e.target.id;
     const computerChoice = randomComputerChoice();
-    const results = finalResults(computerChoice, userChoice);
-  
-    getScore(computerChoice, userChoice);
-  
+
+    displayResults(computerChoice, userChoice);
+    displayScore(computerChoice, userChoice);
+
     const computerAnswer = document.getElementById("computer-choice");
     computerAnswer.innerHTML = computerChoice;
-   
+
     const userAnswer = document.getElementById("user-choice");
     userAnswer.innerHTML = userChoice;
-   
-    const displayResults = document.getElementById("results");
-    displayResults.innerHTML = results;
 }))
 
-//Get a random computerChoice number
 let randomComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 4);
     if (randomNumber === 1) {
@@ -33,41 +28,33 @@ let randomComputerChoice = () => {
     }
 }
 
-// Get results
-function finalResults(computerChoice, userChoice) {
-    if (computerChoice == userChoice) {
-        return "It's a tie!"
-    } else if (computerChoice === 'rock' && userChoice === 'paper') {
-        return 'You Win!'
-    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
-        return 'You lost'
-    } else if (computerChoice === 'paper' && userChoice === 'rock') {
-        return 'You lost'
-    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
-        return 'You win!'
-    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
-        return 'You win!'
-    } else if (computerChoice === 'scissors' && userChoice === 'paper') {
-        return 'You lost'
+function displayResults(computerChoice, userChoice) {
+    const displayResults = document.getElementById("results");
+
+    const gameIsATie = computerChoice == userChoice;
+    if (gameIsATie) {
+        displayResults.innerHTML = "It's a tie!";
+        return;
+    }
+
+    const paperBeatsRock = computerChoice === 'rock' && userChoice === 'paper';
+    const scissorsBeatsPaper = computerChoice === 'paper' && userChoice === 'scissors';
+    const rockBeatsScissors = computerChoice === 'scissors' && userChoice === 'rock';
+    if (paperBeatsRock || scissorsBeatsPaper || rockBeatsScissors) {
+        displayResults.innerHTML = 'You Win!'
+        userScore++;
+    }
+
+    const scissorsLosesRock = computerChoice === 'rock' && userChoice === 'scissors';
+    const paperLosesScissors = computerChoice === 'scissors' && userChoice === 'paper';
+    const rockLosesPaper = computerChoice === 'paper' && userChoice === 'rock'
+    if (scissorsLosesRock || paperLosesScissors || rockLosesPaper) {
+        displayResults.innerHTML = 'You lost'
+        computerScore++;
     }
 }
 
-//Get Scoreboard
-let getScore = (computerChoice, userChoice) => {
-    if (computerChoice === 'rock' && userChoice === 'paper') {
-        userScore++, computerScore + 0;
-    } else if (computerChoice === 'rock' && userChoice === 'scissors') {
-        userScore + 0, computerScore++;
-    } else if (computerChoice === 'paper' && userChoice === 'rock') {
-        userScore + 0, computerScore++;
-    } else if (computerChoice === 'paper' && userChoice === 'scissors') {
-        userScore++, computerScore + 0;
-    } else if (computerChoice === 'scissors' && userChoice === 'rock') {
-        userScore++, computerScore + 0;
-    } else if (computerChoice === 'scissors' && userChoice === 'paper') {
-        userScore + 0, computerScore++;
-    }
-
+let displayScore = () => {
     document.getElementById("computer-score").innerHTML = "Computer Score: " + computerScore;
     document.getElementById("user-score").innerHTML = "Your Score: " + userScore;
 }
